5c05ae9b54c3af5053f6a6ca086ec8486ec8575d
AnthonyLaiuppa <AnthonyLaiuppa@users.noreply.github.com>
2015-03-14 21:58:52 -0400

    Problem 4 Solution Added

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ So far I have taken my approach to solving problems
 - [x] 1 Multiples of 3 and 5  
 - [x] 2 Even Fibonacci Numbers
 - [x] 3 Largest Prime Factor
-- [ ] 4 Largest Palindrome Product 
+- [x] 4 Largest Palindrome Product 
 - [x] 5 Smallest Multiple (I included an alternate solution based on my first approach)
 - [x] 6 Sum Square Difference
 - [x] 7 10001st Prime
---
+++ prob4.py
@@ -0,0 +1,25 @@
+#! usr/bin/env python
+#Check if its the same forwards and back
+def isPal(n):
+    if(str(n)==str(n)[::-1]):
+        return True
+    return False
+
+
+#Big loop of multiplication
+def findPals():
+
+    pals=[]
+    for k in range(100,999):
+        for j in range(100,999):
+            product =k*j
+            if isPal(product):
+                pals.append(product)
+    
+    return(max(pals))
+
+def main():
+
+    print(findPals())
+
+if __name__ == '__main__': main()
